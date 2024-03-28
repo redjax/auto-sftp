91a30c92e1864d99b23c0284a5f0738410567654
redjax <none@none.com>
2024-03-28 01:25:12 -0400

    Start work on deleting oldest backups

--- src/auto_sftp/test.py
+++ src/auto_sftp/test.py
@@ -1,3 +1,4 @@
+import typing as t
 from pathlib import Path
 
 from core import SSHSettings, settings, ssh_settings
@@ -5,6 +6,21 @@ from loguru import logger as log
 from modules import ssh_mod
 from packages import cleanup
 from red_utils.ext.loguru_utils import init_logger, sinks
+import pendulum
+
+from pydantic import BaseModel, Field, ValidationError, field_validator, ConfigDict
+
+
+class File(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    name: str = Field(default=None)
+    path: t.Union[str, Path] = Field(default=None)
+    ext: str = Field(default=None)
+    parent: t.Union[str, Path] = Field(default=None)
+    created_at: pendulum.DateTime = Field(default=None)
+    modified_at: pendulum.DateTime = Field(default=None)
+    size_in_bytes: int = Field(default=0)
 
 
 def extract_file_ext(path: Path = None) -> str:
@@ -66,11 +82,12 @@ def get_file_dicts(files: list[Path] = None) -> list[dict]:
         else:
             f_dict: dict = {
                 "name": f.name,
+                "path": f,
                 "ext": extract_file_ext(f),
                 "parent": f.parent,
-                # "created_at": ...,
-                # "modified_at": ...,
-                # "size": ...
+                "created_at": pendulum.from_timestamp(f.stat().st_ctime),
+                "modified_at": pendulum.from_timestamp(f.stat().st_mtime),
+                "size_in_bytes": f.stat().st_size,
             }
             log.debug(f"File dict: {f_dict}")
             if f not in _dicts:
@@ -89,14 +106,45 @@ def get_file_dicts(files: list[Path] = None) -> list[dict]:
     return _dicts
 
 
+def delete_oldest(files: list[File] = None, threshold: int = 3):
+    sorted_files: list[File] = sorted(files, key=lambda x: x.path.stat().st_ctime)
+
+    delete_count: list[File] = max(0, len(sorted_files) - threshold)
+
+    for f in range(delete_count):
+
+        delete_file: File = sorted_files[f]
+
+        try:
+            log.info(f"Deleting file '{delete_file.path}'.")
+            delete_file.path.unlink()
+            log.success(f"File deleted")
+        except Exception as exc:
+            msg = Exception(
+                f"Unhandled exception deleting file '{delete_file}'. Details: {exc}"
+            )
+            log.error(msg)
+
+            # raise exc
+
+
 if __name__ == "__main__":
     init_logger(sinks=[sinks.LoguruSinkStdErr(level=settings.log_level).as_dict()])
 
     log.info("Running cleanup")
-    files = cleanup.local.list_files(
+    scanned_files = cleanup.local.list_files(
         path=f"{ssh_settings.local_dest}{ssh_settings.extra_path_suffix}"
     )
 
-    f_dicts = get_file_dicts(files)
+    _files: list[File] = []
+
+    f_dicts = get_file_dicts(scanned_files)
     for f in f_dicts:
-        log.debug(f"File: {f}")
+        log.debug(f"File dict: {f}")
+        _file = File.model_validate(f)
+        log.debug(f"File: {_file}")
+        log.debug(f"Test: {_file.model_dump_json()}")
+
+        _files.append(_file)
+
+    delete_oldest(files=_files)
